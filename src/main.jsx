ff98efc250074a7805488d2724d716b54dfe80b6
Joakim Jönsson <jockejockejockejocke@gmail.com>
2023-02-27 10:58:56 +0100

    Created Views/Router.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import About from './views/About/About';
+import Menu from './views/Menu/menu';
+import Status from './views/Status/Status';
+import Profile from './views/Profile/Profile';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/status',
+    element: <Status />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
