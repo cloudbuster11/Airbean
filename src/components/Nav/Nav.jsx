bfdcf60a125ff9ee47d5374f7b496fcb41e71537
Joakim Jönsson <jockejockejockejocke@gmail.com>
2023-02-27 11:02:26 +0100

    Added Components folder.

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+export default function Nav() {
+  return (
+    <nav>
+      <h2>Meny</h2>
+      <ul>
+        <li>Vårt Kaffe</li>
+        <li>Min profil</li>
+        <li>Orderstatus</li>
+        <li>Orderhistorik</li>
+      </ul>
+    </nav>
+  );
+}
