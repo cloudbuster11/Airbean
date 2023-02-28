1c30fbc18c5c3d8bc65d088ed56ea2eb87c6cf2f
tweppy <tweppy@gmail.com>
2023-02-28 13:36:31 +0100

    add nav basic

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,30 @@
+import openNav from '../../assets/navicon.svg';
+import closeNav from '../../assets/close-icon.svg';
+import './Nav.scss';
+import { useRef } from 'react';
+
 export default function Nav() {
+
+  const navRef = useRef();
+
+  function showNav() {
+    navRef.current.classList.toggle('show-nav');
+  }
+
   return (
-    <nav>
-      <h2>Meny</h2>
-      <ul>
-        <li>Vårt Kaffe</li>
-        <li>Min profil</li>
-        <li>Orderstatus</li>
-        <li>Orderhistorik</li>
-      </ul>
-    </nav>
+    <header className='nav__header'>
+      <nav ref={ navRef }>
+        <h2>Meny</h2>
+        <h2>Vårt Kaffe</h2>
+        <h2>Min profil</h2>
+        <h2>Orderstatus</h2>
+        <h2>Orderhistorik</h2>
+      </nav>
+
+      <button className='nav__btn nav__btn--open' onClick={ showNav }>
+          <img src={ openNav } />
+      </button>
+
+    </header>
   );
 }
