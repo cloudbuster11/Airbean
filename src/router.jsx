97e0457561471d4eabb6a6894cea18febd07c4a9
tweppy <tweppy@gmail.com>
2023-03-01 09:45:32 +0100

    landing page = index update

--- src/router.jsx
+++ src/router.jsx
@@ -6,16 +6,10 @@ import Menu from './views/Menu/Menu';
 import Status from './views/Status/Status';
 import Profile from './views/Profile/Profile';
 
-import LandingPage from './views/Index/LandingPage'; //ändra sen
-
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Index />,
-  },
-  {
-    path: '/landing', //ändra sen
-    element: <LandingPage />
   },
   {
     path: '/about',
